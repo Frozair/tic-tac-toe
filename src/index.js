df983da5269a37183508ed3372c37e29fa0669a2
Rudolph Newball <newrdev@gmail.com>
2017-03-17 16:13:17 -0500

    Make the grid and cell looks pretty

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 
 import { AppContainer } from './containers/AppContainer';
 import configureStore from './store/configureStore';
+import './index.css';
 
 const store = configureStore();
 
